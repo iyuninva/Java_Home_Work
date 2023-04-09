aa9c18fc2bde7509eb885200f97ebc47706017a0
IyuninVA <IyuninVA@bk.ru>
2023-04-10 00:01:15 +0400

    add Task_2 lib_4

--- library_4/Task_2.java
+++ library_4/Task_2.java
@@ -6,9 +6,75 @@ package library_4;
 • first() — возвращает первый элемент из очереди, не удаляя.
 */
 
-public class Task_2 {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class Task_2 {
+
+    static LinkedList<Integer> list = new LinkedList<>();
+    static LinkedList<Integer> listEnqueue = new LinkedList<>();
+    static LinkedList<Integer> listDequeue = new LinkedList<>();
+    static LinkedList<Integer> listFirst = new LinkedList<>();
+    static int SIZE = 5;
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        inputList();
+        enqueue();
+        dequeue();
+        first();
     }
 
+    static void inputList() {
+        for (int i = 1; i <= SIZE; i++) {
+            list.add(i);
+        }
+        System.out.printf("Начальний List: %s", list);
+        System.out.println();
+    }
+
+    static void enqueue() {
+        System.out.println();
+        System.out.print("Введите элемент enqueue: ");
+        int temp = scanner();
+        in.close();
+        for (int i = 1; i <= SIZE; i++) {
+            listEnqueue.add(i);
+        }
+        for (int i = 1; i <= SIZE; i++) {
+            if(i == temp) {
+                listEnqueue.addLast(temp);
+                listEnqueue.remove(i-1);
+            }
+        }
+        System.out.printf("enqueue List: %s", listEnqueue);
+        System.out.println();
+    }
+
+    static void dequeue() {
+        for (int i = 1; i <= SIZE; i++) {
+            listDequeue.add(i);
+        }
+        System.out.println();
+        System.out.printf("Возвращенный элемент: %s", listDequeue.removeFirst());
+        System.out.println();
+        System.out.printf("dequeue List: %s", listDequeue);
+        System.out.println();
+    }
+
+    static void first() {
+        for (int i = 1; i <= SIZE; i++) {
+            listFirst.add(i);
+        }
+        System.out.println();
+        System.out.printf("Возвращенный элемент: %s", listFirst.getFirst());
+        System.out.println();
+        System.out.printf("first List: %s", listFirst);
+        System.out.println();
+    }
+
+    public static int scanner() {
+        int inputNumber = in.nextInt();
+        return inputNumber;
+    }
 }
--- library_4/Task_3.java
+++ library_4/Task_3.java
@@ -1,12 +1,107 @@
 package library_4;
 
+import java.util.Scanner;
+
 /*
  * В калькулятор добавьте возможность отменить последнюю операцию.
  */
 
 public class Task_3 {
-    public static void main(String[] args) {
+    static String mode;
+    static int number_1;
+    static int number_2;
+    static int result;
+    static double result_div;
+    static boolean flag = false;
 
+    public static void main(String[] args) {
+        СalMode();
+    }
+
+    static void СalMode() {
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Выберите режим калькулятора ('+' '-' '*' '/') :");
+            mode = in.next();
+            if (mode.equals("+")) {
+                flag = false;
+
+                number_1 = in.nextInt();
+        
+                number_2 = in.nextInt();
+                System.out.printf("%s %s %s = ", number_1, mode, number_2);
+                ModeSum(number_1, number_2);
+                break;
+            } else if (mode.equals("-")) {
+                flag = false;
+                System.out.print("Number - 1 = ");
+                number_1 = in.nextInt();
+                System.out.print("Number - 2 = ");
+                number_2 = in.nextInt();
+                ModeDeduction(number_1, number_2);
+                break;
+            } else if (mode.equals("*")) {
+                flag = false;
+                System.out.print("Number - 1 = ");
+                number_1 = in.nextInt();
+                System.out.print("Number - 2 = ");
+                number_2 = in.nextInt();
+                ModeMultiplier(number_1, number_2);
+                break;
+            } else if (mode.equals("/")) {
+                flag = false;
+                System.out.print("Number - 1 = ");
+                double number_1 = in.nextDouble();
+                System.out.print("Number - 2 = ");
+                double number_2 = in.nextDouble();
+                ModeDivision(number_1, number_2);
+                break;
+            } else {
+                System.out.println("Error: Некорректный ввод");
+            }
+
+        } while (!flag);
+    }
+
+    static int ModeSum(int number_1, int number_2) {
+        result = number_1 + number_2;
+        return PrintOutput(result);
+    }
+
+    static int ModeDeduction(int number_1, int number_2) {
+        result = number_1 - number_2;
+        return PrintOutput(result);
+    }
+
+    static int ModeMultiplier(int number_1, int number_2) {
+        result = number_1 * number_2;
+        return PrintOutput(result);
+    }
+
+    static double ModeDivision(double number_1, double number_2) {
+        if (number_2 == 0) {
+            System.out.println("Error: на 0 делить нельзя!");
+            return 0;
+        } else {
+            if (number_1 % number_2 == 0) {
+                result_div = (number_1 / number_2) + (number_1 % number_2);
+                System.out.println(String.format("%.0f", result_div));
+                return result_div;
+            } else {
+                result_div = (number_1 / number_2) + (number_1 % number_2);
+                return PrintOutputDouble(result_div);
+            }
+        }
+    }
+
+    static int PrintOutput(int result) {
+        System.out.println(result);
+        return result;
+    }
+
+    static double PrintOutputDouble(double result_div) {
+        System.out.println(result_div);
+        return result_div;
     }
 
 }
