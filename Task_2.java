c0897f969eab67b0482183cf08810fcd73f22b50
IyuninVA <IyuninVA@bk.ru>
2023-04-12 23:01:29 +0400

    add_Task_2 lib_5

--- Task_2.java
+++ Task_2.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
 /*
  * Пусть дан список сотрудников:
  * Иван Иванов
@@ -22,5 +26,71 @@
  */
 
 public class Task_2 {
-    
+    static LinkedHashMap<String, String> list = new LinkedHashMap<>();
+    static LinkedHashMap<String, Integer> listSumName = new LinkedHashMap<>();
+    static LinkedHashMap<String, Integer> sortedList = new LinkedHashMap<>();
+
+    static Scanner in = new Scanner(System.in);
+    static int count = 0;
+    static boolean flag;
+
+    static void sortedList() {
+        count = Collections.max(listSumName.values());
+        for (var key : listSumName.keySet()) {
+            for (var keyTemp : listSumName.keySet()) {
+                if(listSumName.get(keyTemp) > count) {
+                    sortedList.put(keyTemp, listSumName.get(keyTemp));
+                }
+            }
+            count--;
+        }
+        System.out.println();
+        System.out.println("Отсортированный список: ");
+        for (var key : sortedList.entrySet()) {
+            System.out.println(key);
+        }
+    }
+
+    static void outputSumName() {
+        for (var key : putList().keySet()) {
+            for (var keyTemp : putList().keySet()) {
+                if ((putList().get(key)).equals(putList().get(keyTemp))) {
+                    count++;
+                    listSumName.put(putList().get(keyTemp), count);
+                }
+            }
+            count = 0;
+        }
+        System.out.println();
+        System.out.println("Начальный список Имен: ");
+        for (var key : listSumName.entrySet()) {
+            System.out.println(key);
+        }
+        sortedList();
+    }
+
+    static LinkedHashMap<String, String> putList() {
+        list.put("Иванов", "Иван");
+        list.put("Петрова", "Светлана");
+        list.put("Белова", "Кристина");
+        list.put("Мусина", "Анна");
+        list.put("Крутова", "Анна");
+        list.put("Юрин", "Иван");
+        list.put("Лыков", "Петр");
+        list.put("Чернов", "Павел");
+        list.put("Федорова", "Мария");
+        list.put("Светлова", "Марина");
+        list.put("Савина", "Мария");
+        list.put("Рыкова", "Мария");
+        list.put("Лугова", "Марина");
+        list.put("Владимирова", "Анна");
+        list.put("Мечников", "Иван");
+        list.put("Петин", "Петр");
+        list.put("Ежов", "Иван");
+        return list;
+    }
+
+    public static void main(String[] args) {
+        outputSumName();
+    }
 }
